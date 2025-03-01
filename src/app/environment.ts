fd3cf18c9cb4e39e4a112326a40b733718571d14
AfonsoMeireles <afonso.msilva17@gmail.com>
2025-03-01 15:56:40 -0400

    Add firebase integration

---
+++ src/app/environment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZEOpExStEmNLS25Rsa7ZOQvI6-hlaRxE",
+  authDomain: "sistemateste-d17ff.firebaseapp.com",
+  projectId: "sistemateste-d17ff",
+  storageBucket: "sistemateste-d17ff.firebasestorage.app",
+  messagingSenderId: "425353399359",
+  appId: "1:425353399359:web:877c33846996b6dc8cfc8a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
